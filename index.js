93015a9aa18182e29f47aa97be460ee3fc217adf
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-12 20:16:55 +0200

    Added 4 array methods with their corresponding tests

---
+++ index.js
@@ -0,0 +1,37 @@
+export const myLength = (array) => {
+    let receivedArray = [...array];
+    let lengthOfArray = 0;
+    receivedArray.forEach(() => {
+        lengthOfArray++;
+    });
+    return lengthOfArray;
+};
+
+export const myPush = (array, valueToAdd) => {
+    let receivedArray = [...array];
+    let newArray = [...receivedArray];
+    newArray[receivedArray.length] = valueToAdd;
+    return newArray;
+};
+
+export const myPop = (array) => {
+    let receivedArray = [...array];
+    let newArray = [];
+    for (let i = 0; i < receivedArray.length - 1; i++) {
+        newArray[i] = receivedArray[i];
+    }
+    return newArray;
+};
+
+export const myShift = (array, valueToAdd) => {
+    let receivedArray = [...array];
+    let newArray = [];
+    myPush(newArray, valueToAdd);
+    newArray[0] = myPush(newArray, valueToAdd)[0];
+    console.log(newArray);
+    for (let i = 0; i < receivedArray.length; i++) {
+        console.log(receivedArray[i]);
+        newArray[i + 1] = receivedArray[i];
+    }
+    return newArray;
+};
